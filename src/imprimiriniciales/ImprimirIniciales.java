7d6365bb3e7109804ee08657f01e8ed74716f55d
erickGP27 <125601903+erickGP27@users.noreply.github.com>
2023-03-09 14:35:01 -0600

    Add files via upload

---
+++ src/imprimiriniciales/ImprimirIniciales.java
@@ -0,0 +1,27 @@
+package imprimiriniciales;
+import java.util.Scanner;
+
+/**
+ *
+ * @author maryse
+ *
+ * Scanner Este programa imprime las iniciales del nombre introducido por el
+ * usuario.
+ *
+ */
+public class ImprimirIniciales {
+
+    public static void main(String[] args) {
+
+        Scanner stdIn = new Scanner(System.in);
+        String nombre; // nombre
+        String apellido; // apellido
+        
+        System.out.print("Introduzca su nombre y primer apellido separados por un espacio: ");
+        nombre = stdIn.next();
+        apellido = stdIn.next();
+        System.out.println("Sus iniciales son " + nombre.charAt(0) + apellido.charAt(0) + ".");
+
+    }
+
+}
